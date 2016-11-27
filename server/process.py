a9edacdf8d1c176bfe3bcb8bab0556cd67999a32
Xue-Min Si <707949748@qq.com>
2016-11-27 11:41:06 +0800

    Update process.py

--- server/process.py
+++ server/process.py
@@ -26,7 +26,7 @@ class check_process(threading.Thread):
 
             for dirpath, dirnames, filenames in os.walk(src):
                 for filename in filenames:
-                    if filename.startswith('MOD') and filename.endswith('.hdf'):                        
+                    if filename.startswith('MOD') and filename.endswith('.hdf'):
                         filelist.append('.'.join(filename.split('.')[0:-1]))
             filelist.sort()
             print(filelist)
@@ -37,10 +37,14 @@ class check_process(threading.Thread):
             print value
             values = value.split(' ')
             print(values)
-            rightlon = float(values[1])
-            leftlon = float(values[0])
-            rightlat = float(values[3])
-            leftlat = float(values[2])
+            EastBoundingCoord = float(values[1])
+            WestBoundingCoord = float(values[0])
+            SouthBoundingCoord = float(values[3])
+            NorthBoundingCoord = float(values[2])
+
+            from transfer import transfer
+            ulx, uly, lrx, lry = transfer(WestBoundingCoord, EastBoundingCoord, NorthBoundingCoord, SouthBoundingCoord)
+
 
             iscrop = True
 
@@ -55,7 +59,7 @@ class check_process(threading.Thread):
                     # os.rename(src_file, dst_file)
 
                     print('preprocess file "%s"......' % filename)
-                    newname, iscrop, crop_name = modisProcessing.main_processing.updateRaster(filename, leftlon, leftlat, rightlon, rightlat)
+                    newname, iscrop, crop_name = modisProcessing.main_processing.updateRaster(filename, ulx, uly, lrx, lry)
 
             zero_mark = False
             if len(filelist) != 0:
@@ -71,7 +75,7 @@ class check_process(threading.Thread):
                 fileset = set()
                 for dripath, dirnames, filenames in os.walk('modisProcessing/MODIS/tiff/arcmapWorkspace/'):
                     for filename in filenames:
-                        if filename.endswith('.tif'):
+                        if filename.split('.')[0].endswith('_crop'):
                             fileset.add(int(filename.split('.')[0].split('_')[0]))
                 fileset = sorted(fileset, reverse=True)
                 print fileset
@@ -80,7 +84,7 @@ class check_process(threading.Thread):
                     if zero_mark and value != self.cu_range:
                         newfilename = str(fileset[0]) + '_CURRENT_RASTER_1000'
                         from modisProcessing import RasterManagement
-                        iscrop, crop_name = RasterManagement.cropandmask(leftlon, leftlat, rightlon, rightlat, newfilename)
+                        iscrop, crop_name = RasterManagement.cropandmask(ulx, uly, lrx, lry, newfilename)
 
                     if iscrop:
                         if os.path.exists('test/'):
